a2a12e1136a831818ec34d80280d0806a1390709
shhio08 <kiiro18.sh@gmail.com>
2024-12-11 14:47:52 +0900

    ex00修正

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,41 +3,41 @@
 int main(void)
 {
 
-    std::cout << "Testing Char: 'a'" << std::endl;
-    ScalarConverter::convert("'a'");
+    // std::cout << "Testing Char: 'a'" << std::endl;
+    // ScalarConverter::convert("'a'");
 
-    std::cout << "\nTesting Int: 42" << std::endl;
-    ScalarConverter::convert("42");
+    // std::cout << "\nTesting Int: 42" << std::endl;
+    // ScalarConverter::convert("42");
 
-    std::cout << "\nTesting Float: 42.42f" << std::endl;
-    ScalarConverter::convert("42.42f");
+    // std::cout << "\nTesting Float: 42.42f" << std::endl;
+    // ScalarConverter::convert("42.42f");
 
-    std::cout << "\nTesting Double: 42.42" << std::endl;
-    ScalarConverter::convert("42.42");
+    // std::cout << "\nTesting Double: 42.42" << std::endl;
+    // ScalarConverter::convert("42.42");
 
-    std::cout << "\nTesting Special Float: nanf" << std::endl;
-    ScalarConverter::convert("nanf");
+    // std::cout << "\nTesting Special Float: nanf" << std::endl;
+    // ScalarConverter::convert("nanf");
 
-    std::cout << "\nTesting Special Double: nan" << std::endl;
-    ScalarConverter::convert("nan");
+    // std::cout << "\nTesting Special Double: nan" << std::endl;
+    // ScalarConverter::convert("nan");
 
-    std::cout << "\nTesting Float: +inff" << std::endl;
-    ScalarConverter::convert("+inff");
+    // std::cout << "\nTesting Float: +inff" << std::endl;
+    // ScalarConverter::convert("+inff");
 
-    std::cout << "\nTesting Double: -inf" << std::endl;
-    ScalarConverter::convert("-inf");
+    // std::cout << "\nTesting Double: -inf" << std::endl;
+    // ScalarConverter::convert("-inf");
 
-    std::cout << "\nTesting Invalid Char: 'ab'" << std::endl;
-    ScalarConverter::convert("'ab'");
+    // std::cout << "\nTesting Invalid Char: 'ab'" << std::endl;
+    // ScalarConverter::convert("'ab'");
 
-    std::cout << "\nTesting Invalid Int: 42.42" << std::endl;
-    ScalarConverter::convert("42.42");
+    // std::cout << "\nTesting Invalid Int: 42.42" << std::endl;
+    // ScalarConverter::convert("42.42");
 
-    std::cout << "\nTesting Invalid Float: 42.42" << std::endl;
-    ScalarConverter::convert("42.42");
+    // std::cout << "\nTesting Invalid Float: 42.42" << std::endl;
+    // ScalarConverter::convert("42.42");
 
-    std::cout << "\nTesting Invalid Double: 42.42.42" << std::endl;
-    ScalarConverter::convert("42.42.42");
+    // std::cout << "\nTesting Invalid Double: 42.42.42" << std::endl;
+    // ScalarConverter::convert("42.42.42");
 
 	//pdfのテスト
 	std::cout << std::endl << std::endl;
